b7e7863f9d292dbc51f033be0610038b8bafe05a
Emmanouil "Manolis" Maragkakis <mns.marag@gmail.com>
2014-03-12 18:09:36 -0400

    Migrate script count_reads_on_genic_elements to MooseX::App

--- lib/CLIPSeqTools/App/count_reads_on_genic_elements.pm
+++ lib/CLIPSeqTools/App/count_reads_on_genic_elements.pm
@@ -1,38 +1,10 @@
 =head1 NAME
 
-CLIPSeqTools::App::Command::count_reads_on_genic_elements - Count library reads on transcripts, genes, exons, introns
+CLIPSeqTools::App::count_reads_on_genic_elements - Count library reads on transcripts, genes, exons, introns
 
 =head1 SYNOPSIS
 
 count_tags_per_genic_element.pl [options/parameters]
-
-Counts library reads on transcripts, genes, exons, introns.
-
-  Input options for library.
-    -type <Str>            input type (eg. DBIC, BED).
-    -file <Str>            input file. Only works if type specifies a file type.
-    -driver <Str>          driver for database connection (eg. mysql, SQLite). Only works if type is DBIC.
-    -database <Str>        database name or path to database file for file based databases (eg. SQLite). Only works if type is DBIC.
-    -table <Str>           database table. Only works if type is DBIC.
-    -host <Str>            hostname for database connection. Only works if type is DBIC.
-    -user <Str>            username for database connection. Only works if type is DBIC.
-    -password <Str>        password for database connection. Only works if type is DBIC.
-    -records_class <Str>   type of records stored in database (Default: GenOO::Data::DB::DBIC::Species::Schema::SampleResultBase::v3).
-    -filter <Filter>       filter library. Option can be given multiple times.
-                             Filter syntax: column_name="pattern"
-                               e.g. -filter deletion="def" -filter rmsk="undef" to keep reads with deletions and not repeat masked.
-                               e.g. -filter query_length=">31" -filter query_length="<=50" to keep reads longer than 31 and shorter or   equal to 50.
-                             Supported operators: ">", ">=", "<", "<=", "=", "!=","def", "undef"
-
-  Other input
-    -gtf <Str>             GTF file with genes/transcripts.
-
-  Output
-    -o_prefix <Str>        output path prefix. Script adds an extension to path. If path does not exist it will be created. Default: ./
-
-  Other options.
-    -v --verbose           print progress lines and extra information.
-    -h -? --usage --help   print help message
 
 =head1 DESCRIPTION
 
@@ -41,10 +13,49 @@ Counts library reads on transcripts, genes, exons, introns.
 * Transcript counts are measured only on their exons.
 * Gene counts are measured only for exonic regions.
 
+=head1 OPTIONS
+
+Input options for library.
+    -type <Str>            input type (eg. DBIC, BED).
+    -file <Str>            input file. Only works if type specifies a file type.
+    -driver <Str>          driver for database connection (eg. mysql,
+                           SQLite).
+    -database <Str>        database name or path to database file for file
+                           based databases (eg. SQLite).
+    -table <Str>           database table.
+    -host <Str>            hostname for database connection.
+    -user <Str>            username for database connection.
+    -password <Str>        password for database connection.
+    -records_class <Str>   type of records stored in database.
+    -filter <Filter>       filter library. May be used multiple times.
+                           Syntax: column_name="pattern"
+                           e.g. keep reads with deletions AND not repeat
+                                masked AND longer than 31
+                                -filter deletion="def" 
+                                -filter rmsk="undef" .
+                                -filter query_length=">31".
+                           Operators: >, >=, <, <=, =, !=, def, undef
+
+  Other input
+    -gtf <Str>             GTF file with genes/transcripts.
+
+  Output
+    -o_prefix <Str>        output path prefix. Script will create and add
+                           extension to path. Default: ./
+
+  Other options.
+    -v --verbose           print progress lines and extra information.
+    -h -? --usage --help   print help message
+
 =cut
 
 
-package CLIPSeqTools::App::Command::count_reads_on_genic_elements;
+package CLIPSeqTools::App::count_reads_on_genic_elements;
+
+
+# Make it an app command
+use MooseX::App::Command;
+extends 'CLIPSeqTools::App';
 
 
 #######################################################################
@@ -56,9 +67,6 @@ use Moose;
 use namespace::autoclean;
 use File::Spec;
 use List::Util qw(sum max);
-
-
-extends 'MooseX::App::Cmd::Command'; # Declare that class is a command
 
 
 #######################################################################
@@ -80,32 +88,26 @@ with
 	"CLIPSeqTools::Role::VerbosityOption" => {
 		-alias    => { validate_args => '_validate_args_for_verbosity_option' },
 		-excludes => 'validate_args',
-	},
-	"CLIPSeqTools::Role::HelpOption" => {
-		-alias    => { validate_args => '_check_help_flag' },
-		-excludes => 'validate_args',
 	};
 
 	
 #######################################################################
 ########################   Interface Methods   ########################
 #######################################################################
-sub description {
-	'Read counts on genic elements (ie. transcripts, genes, exons, introns)';
-}
-
 sub validate_args {
 	my ($self, $opt, $args) = @_;
 	
-	$self->_check_help_flag;
 	$self->_validate_args_for_reads_collection_input;
 	$self->_validate_args_for_genecollection_input;
 	$self->_validate_args_for_output_prefix_option;
 	$self->_validate_args_for_verbosity_option;
 }
 
-sub execute {
+sub run {
 	my ($self, $opt, $args) = @_;
+	
+	warn "Validating arguments\n" if $self->verbose;
+	$self->validate_args();
 	
 	warn "Creating gene collection\n" if $self->verbose;
 	my $gene_collection = $self->gene_collection;
