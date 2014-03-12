840475e3f38e1a2f654ce02f7c9146c3ede5b2c6
Emmanouil "Manolis" Maragkakis <mns.marag@gmail.com>
2014-03-12 17:57:22 -0400

    Migrate genomic_distribution to MooseX::App

--- lib/CLIPSeqTools/App/genomic_distribution.pm
+++ lib/CLIPSeqTools/App/genomic_distribution.pm
@@ -1,41 +1,53 @@
 =head1 NAME
 
-CLIPSeqTools::App::Command::genomic_distribution - Count reads on genes, repeats, exons, etc.
+CLIPSeqTools::App::genomic_distribution - Count reads on genes, repeats, exons, etc.
 
 =head1 SYNOPSIS
 
 genomic_distribution.pl [options/parameters]
 
-Measure the number of reads that align to each genome wide annotation (e.g. genic, intergenic, repeats, exonic, intronic, etc).
+=head1 DESCRIPTION
+
+Measure the number of reads that align to each genome wide annotation
+such as genic, intergenic, repeats, exonic, intronic, etc.
+
+=head1 OPTIONS
 
   Input options for library.
-    -driver <Str>          driver for database connection (eg. mysql, SQLite).
-    -database <Str>        database name or path to database file for file based databases (eg. SQLite).
+    -driver <Str>          driver for database connection (eg. mysql,
+                           SQLite).
+    -database <Str>        database name or path to database file for file
+                           based databases (eg. SQLite).
     -table <Str>           database table.
     -host <Str>            hostname for database connection.
     -user <Str>            username for database connection.
     -password <Str>        password for database connection.
-    -records_class <Str>   type of records stored in database (Default: GenOO::Data::DB::DBIC::Species::Schema::SampleResultBase::v3).
-    -filter <Filter>       filter library. Option can be given multiple times.
-                           Filter syntax: column_name="pattern"
-                             e.g. -filter deletion="def" -filter rmsk="undef" to keep reads with deletions and not repeat masked.
-                             e.g. -filter query_length=">31" -filter query_length="<=50" to keep reads longer than 31 and shorter or   equal to 50.
-                           Supported operators: ">", ">=", "<", "<=", "=", "!=","def", "undef"
+    -records_class <Str>   type of records stored in database.
+    -filter <Filter>       filter library. May be used multiple times.
+                           Syntax: column_name="pattern"
+                           e.g. keep reads with deletions AND not repeat
+                                masked AND longer than 31
+                                -filter deletion="def" 
+                                -filter rmsk="undef" .
+                                -filter query_length=">31".
+                           Operators: >, >=, <, <=, =, !=, def, undef
 
   Output
-    -o_prefix <Str>        output path prefix. Script adds an extension to path. If path does not exist it will be created. Default: ./
+    -o_prefix <Str>        output path prefix. Script will create and add
+                           extension to path. Default: ./
 
   Other options.
     -v --verbose           print progress lines and extra information.
     -h -? --usage --help   print help message
 
-=head1 DESCRIPTION
-
-Measure the number of reads that align to each genome wide annotation (e.g. genic, intergenic, repeats, exonic, intronic, etc).
-
 =cut
 
-package CLIPSeqTools::App::Command::genomic_distribution;
+package CLIPSeqTools::App::genomic_distribution;
+
+
+# Make it an app command
+use MooseX::App::Command;
+extends 'CLIPSeqTools::App';
 
 
 #######################################################################
@@ -43,13 +55,9 @@ package CLIPSeqTools::App::Command::genomic_distribution;
 #######################################################################
 use Modern::Perl;
 use autodie;
-use Moose;
 use namespace::autoclean;
 use File::Spec;
 use List::Util qw(sum max);
-
-
-extends 'MooseX::App::Cmd::Command'; # Declare that class is a command
 
 
 #######################################################################
@@ -67,43 +75,42 @@ with
 	"CLIPSeqTools::Role::VerbosityOption" => {
 		-alias    => { validate_args => '_validate_args_for_verbosity_option' },
 		-excludes => 'validate_args',
-	},
-	"CLIPSeqTools::Role::HelpOption" => {
-		-alias    => { validate_args => '_check_help_flag' },
-		-excludes => 'validate_args',
 	};
 
 
 #######################################################################
 ###################   Silence command line options   ##################
 #######################################################################
-has '+type' => (
-	traits        => ['NoGetopt']
+option 'type' => (
+	is            => 'ro',
+	isa           => 'Str',
+	default       => 'DBIC',
+	documentation => 'input type (eg. DBIC, BED, SAM).',
 );
 
-has '+file' => (
-	traits        => ['NoGetopt']
+option 'file' => (
+	is            => 'ro',
+	isa           => 'Str',
+	documentation => 'input file. Only works if type specifies a file type.',
 );
 
 
 #######################################################################
 ########################   Interface Methods   ########################
 #######################################################################
-sub description {
-	q(Measure the number of reads that align to each genome wide annotation (e.g. genic, intergenic, repeats, exonic, intronic, etc).);
-}
-
 sub validate_args {
 	my ($self, $opt, $args) = @_;
 	
-	$self->_check_help_flag;
 	$self->_validate_args_for_reads_collection_input;
 	$self->_validate_args_for_output_prefix_option;
 	$self->_validate_args_for_verbosity_option;
 }
 
-sub execute {
+sub run {
 	my ($self, $opt, $args) = @_;
+	
+	warn "Validating arguments\n" if $self->verbose;
+	$self->validate_args();
 	
 	warn "Creating reads collection\n" if $self->verbose;
 	my $reads_collection = $self->reads_collection;
