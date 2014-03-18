00e3b99888cf05f63c3ea21fb9d9ac758dc9f357
Emmanouil "Manolis" Maragkakis <mns.marag@gmail.com>
2014-03-18 13:38:41 -0400

    Migrate script plot_libraries_relative_read_density to PlotApp (MooseX::App)

---
+++ lib/CLIPSeqTools/PlotApp/libraries_relative_read_density.pm
@@ -0,0 +1,96 @@
+=head1 NAME
+
+CLIPSeqTools::PlotApp::libraries_relative_read_density - Create plots for script libraries_relative_read_density.
+
+=head1 SYNOPSIS
+
+clipseqtools libraries_relative_read_density [options/parameters]
+
+=head1 DESCRIPTION
+
+Create plots for script libraries_relative_read_density.
+
+=head1 OPTIONS
+
+  Input.
+    -file <Str>            input file.
+
+  Output
+    -o_prefix <Str>        output path prefix. Script will create and add
+                           extension to path. Default: ./
+
+    -v --verbose           print progress lines and extra information.
+    -h -? --usage --help   print help message
+    
+=cut
+
+package CLIPSeqTools::PlotApp::libraries_relative_read_density;
+
+
+# Make it an app command
+use MooseX::App::Command;
+extends 'CLIPSeqTools::PlotApp';
+
+
+#######################################################################
+#######################   Load External modules   #####################
+#######################################################################
+use Modern::Perl;
+use autodie;
+use namespace::autoclean;
+use File::Spec;
+
+
+#######################################################################
+#######################   Command line options   ######################
+#######################################################################
+option 'file' => (
+	is            => 'rw',
+	isa           => 'Str',
+	required      => 1,
+	documentation => 'input file.',
+);
+
+#######################################################################
+##########################   Consume Roles   ##########################
+#######################################################################
+with 
+	"CLIPSeqTools::Role::OutputPrefixOption" => {
+		-alias    => { validate_args => '_validate_args_for_output_prefix_option' },
+		-excludes => 'validate_args',
+	},
+	"CLIPSeqTools::Role::VerbosityOption" => {
+		-alias    => { validate_args => '_validate_args_for_verbosity_option' },
+		-excludes => 'validate_args',
+	};
+
+	
+#######################################################################
+########################   Interface Methods   ########################
+#######################################################################
+sub validate_args {
+	my ($self) = @_;
+	
+	$self->_validate_args_for_output_prefix_option;
+	$self->_validate_args_for_verbosity_option;
+}
+
+sub run {
+	my ($self) = @_;
+	
+	warn "Validating arguments\n" if $self->verbose;
+	$self->validate_args();
+	
+	warn "Creating output path\n" if $self->verbose;
+	$self->make_path_for_output_prefix();
+	
+	warn "Building output file\n" if $self->verbose;
+	my (undef, undef, $filename) = File::Spec->splitpath($self->file);
+	$filename =~ s/\.tab$//;
+	my $figfile = $self->o_prefix . $filename . '.pdf';
+	
+	warn "Creating plots\n" if $self->verbose;
+	system q{Rscript bin/plot_libraries_relative_read_density.R --ifile=} . $self->file . q{ --figfile=} . $figfile;
+}
+
+1;
