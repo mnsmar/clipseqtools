d6d7ded9d0b4d16acef6a24e116fc07160434765
Emmanouil "Manolis" Maragkakis <mns.marag@gmail.com>
2014-06-20 18:31:26 -0400

    Correct bug (divide by 0) in uq normalization.
    Some times when one library was orders of magnitude larger than another it
    would have much more elements (genes, exons, etc) with non-zero values. In
    these cases the index of the 25th percentile element would result in getting a
    0 value from the smallest table (smallest library). This would create a
    division by 0 error during normalization. Now the 25th percentile index is
    calculated from the smallest table.

--- lib/CLIPSeqTools/CompareApp/upper_quartile_normalization.pm
+++ lib/CLIPSeqTools/CompareApp/upper_quartile_normalization.pm
@@ -1,6 +1,7 @@
 =head1 NAME
 
-CLIPSeqTools::CompareApp::upper_quartile_normalization - Do Upper Quartile normalization on specified columns of tables.
+CLIPSeqTools::CompareApp::upper_quartile_normalization - Do Upper Quartile
+normalization on specified columns of tables.
 
 =head1 SYNOPSIS
 
@@ -8,10 +9,11 @@ clipseqtools upper_quartile_normalization [options/parameters]
 
 =head1 DESCRIPTION
 
-Do Upper Quartile normalization on specified value column of one or more tables.
-For each table, all entries are sorted by descending value (specified by val_col).
-All values are divided by the value of the 25%th entry.
-The 25%th entry is found after the entries with value lower than val_thres in all tables are excluded.
+Do Upper Quartile normalization on specified value column of one or more
+tables.  For each table, all entries are sorted by descending value (specified
+by val_col).  All values are divided by the value of the 25%th entry.  The
+25%th entry is specified by the table with the fewer entries after the entries
+with value lower than val_thres in all tables are excluded.
 
 =head1 OPTIONS
 
@@ -53,6 +55,7 @@ use namespace::autoclean;
 use File::Spec;
 use File::Path qw(make_path);
 use Data::Table;
+use List::Util qw(min);
 
 
 #######################################################################
@@ -62,14 +65,17 @@ option 'table' => (
 	is            => 'rw',
 	isa           => 'ArrayRef[Str]',
 	required      => 1,
-	documentation => 'input table file/files. Use option multiple times to give multiple files.',
+	documentation => 'input table file/files. Use option multiple times to '.
+						'give multiple files.',
 );
 
 option 'key_col' => (
 	is            => 'rw',
 	isa           => 'ArrayRef[Str]',
 	required      => 1,
-	documentation => 'name for the column/columns to use as a key. It must be unique for each table row. Use option multiple times to specify multiple columns.',
+	documentation => 'name for the column/columns to use as a key. It must '.
+						'be unique for each table row. Use option multiple '.
+						'times to specify multiple columns.',
 );
 
 option 'val_col' => (
@@ -83,14 +89,17 @@ option 'val_thres' => (
 	is            => 'rw',
 	isa           => 'Num',
 	default       => 0,
-	documentation => 'rows with value lower or equal than val_thres are not used for normalization.',
+	documentation => 'rows with value lower or equal than val_thres are not '.
+						'used for normalization.',
 );
 
 option 'o_table' => (
 	is            => 'rw',
 	isa           => 'ArrayRef[Str]',
 	required      => 1,
-	documentation => 'output table file/files. Use option multiple times to give multiple files. Must be given as many times as the table option.'
+	documentation => 'output table file/files. Use option multiple times to '.
+						'give multiple files. Must be given as many times '.
+						'as the table option.'
 );
 
 
@@ -99,33 +108,36 @@ option 'o_table' => (
 #######################################################################
 sub validate_args {
 	my ($self) = @_;
-	
-	$self->usage_error('Input table files must be as many as output ones.') if @{$self->table} != @{$self->o_table};
+
+	$self->usage_error('Input table files must be as many as output ones.')
+		if @{$self->table} != @{$self->o_table};
 }
 
 sub run {
 	my ($self) = @_;
-	
+
 	warn "Starting analysis: upper_quartile_normalization\n";
-	
+
 	warn "Validating arguments\n" if $self->verbose;
 	$self->validate_args();
-	
+
 	warn "Reading input files\n" if $self->verbose;
 	my @tables = map {Data::Table::fromFile($_)} @{$self->table};
 	die "Table sizes differ\n" if not all_tables_of_equal_size(@tables);
 
-	warn "Calculating number of unique records with value over threshold\n" if $self->verbose;
-	my $records_above_thres = _count_unique_records_with_value_over_thres($self->val_col, $self->val_thres, $self->key_col, @tables);
-	my $quantile_index = int($records_above_thres * 0.25);
+	warn "Calculating number of unique records with value over threshold\n"
+		if $self->verbose;
+	my $uq_idx = _quantile_idx_from_table_with_fewer(
+		$self->val_col, $self->val_thres, $self->key_col, @tables);
 
 	warn "Normalizing the values\n" if $self->verbose;
-	_build_normalized_column_in_tables($self->val_col, $quantile_index, @tables); #Normalized column name is value column + "_uq" suffix
-	
+	#Normalized column name is value column + "_uq" suffix
+	_build_normalized_column_in_tables($self->val_col, $uq_idx, @tables);
+
 	warn "Writing output files\n" if $self->verbose;
 	for (my $i=0; $i<@{$self->o_table}; $i++) {
-		my (undef, $directory, undef) = File::Spec->splitpath($self->o_table->[$i]);
-		make_path($directory);
+		my (undef, $dir, undef) = File::Spec->splitpath($self->o_table->[$i]);
+		make_path($dir);
 		open (my $OUT, '>', $self->o_table->[$i]);
 		print $OUT $tables[$i]->tsv;
 		close $OUT;
@@ -137,49 +149,55 @@ sub run {
 ############################   Functions   ############################
 #######################################################################
 sub _build_normalized_column_in_tables {
-	my ($value_column, $quantile_index, @tables) = @_;
-	
+	my ($value_col, $uq_idx, @tables) = @_;
+
 	# Sort the tables by descending value
-	map {$_->sort($value_column, Data::Table::NUMBER, Data::Table::DESC)} @tables;
-	
+	map{$_->sort($value_col, Data::Table::NUMBER, Data::Table::DESC)} @tables;
 	foreach my $table (@tables) {
-		my $uq = $table->elm($quantile_index, $value_column);
-		my @normalized_values = map {$_ / $uq} $table->col($value_column);
-		$table->addCol(\@normalized_values, $value_column."_uq");
+		my $uq = $table->elm($uq_idx, $value_col);
+		my @normalized_values = map {$_ / $uq} $table->col($value_col);
+		$table->addCol(\@normalized_values, $value_col."_uq");
 	}
 }
 
-sub _count_unique_records_with_value_over_thres {
-	my ($value_column, $val_thres, $key_columns, @tables) = @_;
-	
-	# Sort the tables according to the keys
-	my @sorting_conditions = map{$_, Data::Table::STRING, Data::Table::ASC} @{$key_columns};
-	map {$_->sort(@sorting_conditions)} @tables;
-	
-	my $records_count = 0;
-	my $row_count = $tables[0]->nofRow;
-	for (my $i=0; $i<$row_count; $i++) {
-		my @values = map {$_->elm($i, $value_column)} @tables;
-		if ((grep {$_ > $val_thres} @values) > 0) {
-			$records_count++;
+sub _quantile_idx_from_table_with_fewer {
+	my ($value_col, $val_thres, $key_cols, @tables) = @_;
+
+	# Sort tables according to keys
+	my @rule = map{$_, Data::Table::STRING, Data::Table::ASC} @{$key_cols};
+	map {$_->sort(@rule)} @tables;
+
+	my @rec_counts;
+	foreach my $table (@tables) {
+		my $records_count = 0;
+		my $row_count = $table->nofRow;
+		for (my $i=0; $i<$row_count; $i++) {
+			my $value = $table->elm($i, $value_col);
+			if ($value > $val_thres) {
+				$records_count++;
+			}
 		}
+		push @rec_counts, $records_count;
 	}
-	
-	return $records_count;
+
+	my $min_count = min(@rec_counts);
+	my $quantile_idx = int($min_count * 0.25);
+
+	return $quantile_idx;
 }
 
 sub all_tables_of_equal_size {
 	my @tables = @_;
-	
+
 	my $row_count = $tables[0]->nofRow;
 	my $col_count = $tables[0]->nofCol;
-	
+
 	foreach my $table (@tables) {
 		if ($table->nofRow != $row_count or $table->nofCol != $col_count) {
 			return 0;
 		}
 	}
-	
+
 	return 1;
 }
 
